9dd38f8c19117a2ba227de09b61f301f8db489cf
LucasJohnNyamhanga <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-06-07 10:24:02 +0300

    gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "98543b6f6e7762d8"; }
+/******/ 		__webpack_require__.h = function() { return "2cdce7dca20c0dbd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.7b862d8e986a1b73.hot-update.js
+++ .next/static/webpack/webpack.7b862d8e986a1b73.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "97a864b0e8f86f50"; }
+/******/ 	__webpack_require__.h = function() { return "ca750b2fedec7d49"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
