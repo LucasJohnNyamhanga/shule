f2d513d1d26472e2c544542f7e761d997e5183e9
LucasJohnNyamhanga <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-06-15 17:32:49 +0300

    clean hero

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
 import Styles from '../../styles/footer.module.scss';
 
 const Footer = () => {
 	return (
 		<div className={Styles.container}>
-			<div className={Styles.innerContainer}>SOME CONTENT</div>
+			<div className={Styles.innerContainer}>
+				<div className={Styles.footerDetails}>
+					<div className={Styles.header}> LINKS</div>
+					<div className={Styles.body}>Notes</div>
+					<div className={Styles.body}>Review</div>
+					<div className={Styles.body}>Exams</div>
+					<div className={Styles.body}>References</div>
+				</div>
+				<div className={Styles.footerDetails}>
+					<div className={Styles.header}>About Us</div>
+				</div>
+				<div className={Styles.footerDetails}>
+					<div className={Styles.header}>Guide lines</div>
+				</div>
+
+				<div className={Styles.footerDetails}>
+					<div className={Styles.header}>Social Media</div>
+				</div>
+			</div>
+			<div className={Styles.copyrights}>www.shule.co.tz</div>
 		</div>
 	);
 };
