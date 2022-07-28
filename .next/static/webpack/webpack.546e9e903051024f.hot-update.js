a7f29ab42921133bc44688e51b6b10ba0e122077
LucasJohnNyamhanga <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-07-28 12:07:00 +0300

    footer update

--- .next/static/webpack/webpack.546e9e903051024f.hot-update.js
+++ .next/static/webpack/webpack.546e9e903051024f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "4153ba577dfeebc1"; }
+/******/ 	__webpack_require__.h = function() { return "a5a9ab766ffdf0f2"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
