a9108c2e740c7c09f2db31e80f162c0b375813e5
LucasJohnNyamhanga <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-08-16 09:31:28 +0300

    varcel

--- prisma/migrations/20220726105236_userbyname/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `users_name_username_idx` ON `users`;
-
--- CreateIndex
-CREATE FULLTEXT INDEX `users_username_name_idx` ON `users`(`username`, `name`);
--- prisma/migrations/20220803124150_order_status/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `order` ADD COLUMN `status` BOOLEAN NOT NULL DEFAULT true;
