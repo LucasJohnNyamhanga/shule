d4e294b52c40aeacfe75d9534cbe189f7e78df35
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-14 14:58:11 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Shule</title>
+        <meta
+          key="desc"
+          name="description"
+          content={`Get the best Notes, Answered Past Papers, Online Quiz and A full Books Library for O level and A level studies for all subjects. - Download Now`}
+        />
         <meta
           name="google-site-verification"
           content="oBwl1w3_NlylQ2IpnH8227Zl6J-0vatDGgFYPsvl1zs"
