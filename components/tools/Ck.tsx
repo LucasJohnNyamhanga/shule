50a96d34e73b5fa51be8382022765027829f7554
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-18 18:18:41 +0300

    misunderstandings

--- components/tools/Ck.tsx
+++ components/tools/Ck.tsx
@@ -1,104 +1,104 @@
-import React, { Component, useEffect, useState } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from 'ckeditor5-build-classic-extended';
-import Styles from '../../styles/ckEditor.module.scss';
+import React, { Component, useEffect, useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "ckeditor5-build-classic-extended";
+import Styles from "../../styles/ckEditor.module.scss";
 
 type dataContent = {
-	content: (data: string) => void;
-	dataCk: string;
-	onReadyToStart: () => void;
+  content: (data: string) => void;
+  dataCk: string;
+  onReadyToStart: () => void;
 };
 
 const Ck = ({ content, dataCk, onReadyToStart }: dataContent) => {
-	const [data, setData] = useState(dataCk);
+  const [data, setData] = useState(dataCk);
 
-	useEffect(() => {
-		setData(dataCk);
-	}, []);
+  useEffect(() => {
+    setData(dataCk);
+  }, []);
 
-	return (
-		<div className={Styles.container}>
-			<CKEditor
-				editor={ClassicEditor}
-				data={data}
-				onReady={onReadyToStart}
-				onChange={(event: any, editor: any) => {
-					const data = editor.getData();
-					content(data);
-				}}
-				onBlur={(event: any, editor: any) => {
-					console.log('Blur.', editor);
-				}}
-				onFocus={(event: any, editor: any) => {
-					console.log('Focus.', editor);
-				}}
-				config={{
-					toolbar: {
-						items: [
-							'heading',
-							'|',
-							'fontfamily',
-							'fontsize',
-							'|',
-							'alignment',
-							'|',
-							'fontColor',
-							'fontBackgroundColor',
-							'|',
-							'bold',
-							'italic',
-							'strikethrough',
-							'underline',
-							'subscript',
-							'superscript',
-							'|',
-							'link',
-							'|',
-							'outdent',
-							'indent',
-							'|',
-							'bulletedList',
-							'numberedList',
-							'todoList',
-							'|',
-							'code',
-							'codeBlock',
-							'blockQuote',
-							'|',
-							'link',
-							'imageUpload',
-							'|',
-							'insertTable',
-							'specialCharacters',
-							'tableColumn',
-							'tableRow',
-							'mergeTableCells',
-							'mediaEmbed',
-							'|',
-							'undo',
-							'redo',
-						],
-						shouldNotGroupWhenFull: true,
-					},
+  return (
+    <div className={Styles.container}>
+      <CKEditor
+        editor={ClassicEditor}
+        data={data}
+        onReady={onReadyToStart}
+        onChange={(event: any, editor: any) => {
+          const data = editor.getData();
+          content(data);
+        }}
+        onBlur={(event: any, editor: any) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(event: any, editor: any) => {
+          console.log("Focus.", editor);
+        }}
+        config={{
+          toolbar: {
+            items: [
+              "heading",
+              "|",
+              "fontfamily",
+              "fontsize",
+              "|",
+              "alignment",
+              "|",
+              "fontColor",
+              "fontBackgroundColor",
+              "|",
+              "bold",
+              "italic",
+              "strikethrough",
+              "underline",
+              "subscript",
+              "superscript",
+              "|",
+              "link",
+              "|",
+              "outdent",
+              "indent",
+              "|",
+              "bulletedList",
+              "numberedList",
+              "todoList",
+              "|",
+              "code",
+              "codeBlock",
+              "blockQuote",
+              "|",
+              "link",
+              "imageUpload",
+              "|",
+              "insertTable",
+              "specialCharacters",
+              "tableColumn",
+              "tableRow",
+              "mergeTableCells",
+              "mediaEmbed",
+              "|",
+              "undo",
+              "redo",
+            ],
+            shouldNotGroupWhenFull: true,
+          },
 
-					simpleUpload: {
-						// The URL that the images are uploaded to.
-						// uploadUrl: 'http://127.0.0.1:8000/api/uploadCk',
-						uploadUrl: 'https://www.database.co.tz/api/uploadCk',
-						// uploadUrl: 'http://localhost:3000/api/uploadCkReDirect',
-						// withCredentials: true,
-						// headers: {
-						// 	'X-CSRF-TOKEN': 'CSRF-Token',
-						// 	Authorization: 'Bearer <JSON Web Token>',
-						// },
-					},
-				}}
-			/>
-		</div>
-	);
+          simpleUpload: {
+            // The URL that the images are uploaded to.
+            // uploadUrl: 'http://127.0.0.1:8000/api/uploadCk',
+            uploadUrl: "https://www.database.co.tz/api/uploadCk",
+            // uploadUrl: 'http://localhost:3000/api/uploadCkReDirect',
+            // withCredentials: true,
+            // headers: {
+            // 	'X-CSRF-TOKEN': 'CSRF-Token',
+            // 	Authorization: 'Bearer <JSON Web Token>',
+            // },
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default Ck;
 function handleEditor() {
-	throw new Error('Function not implemented.');
+  throw new Error("Function not implemented.");
 }
--- pages/api/uploadCk.tsx
+++ pages/api/uploadCk.tsx
@@ -1,44 +1,38 @@
-
-import middleware from '../../components/middleware/middleware'
-import nextConnect from 'next-connect';
-var mv = require('mv');
-import type { NextApiRequest, NextApiResponse } from 'next'
+import middleware from "../../components/middleware/middleware";
+import nextConnect from "next-connect";
+var mv = require("mv");
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = nextConnect();
 
 handler.use(middleware);
 
 type Data = {
-    uploaded:boolean,
-    url: string
-  }
+  uploaded: boolean;
+  url: string;
+};
 
-handler.post(async (
-    req: NextApiRequest,
-  res: NextApiResponse<Data>
-) => {
-	try {
-        const {files}:any = req;
-        const body = req.body;
-        const file = JSON.parse(JSON.stringify(files));
-        var oldPath =file.upload.filepath;
-        var newPath = `./public/uploads/${file.upload.originalFilename }`;
-        mv(oldPath, newPath, function (err:any) {
-            console.log(err)
-        });
-        res.status(200).json({
-            uploaded: true,
-            url:`/uploads/${file.upload.originalFilename }`
-        })
-	} catch (err) {
-		
-	}
+handler.post(async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    const { files }: any = req;
+    const body = req.body;
+    const file = JSON.parse(JSON.stringify(files));
+    var oldPath = file.upload.filepath;
+    var newPath = `./public/uploads/${file.upload.originalFilename}`;
+    mv(oldPath, newPath, function (err: any) {
+      console.log(err);
+    });
+    res.status(200).json({
+      uploaded: true,
+      url: `/uploads/${file.upload.originalFilename}`,
+    });
+  } catch (err) {}
 });
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
 
 export default handler;
