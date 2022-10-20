abe2902f3680a12ccee7db833b1b3c9ced55c014
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-20 19:50:06 +0300

    trial final pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
 	register: true,
+	skipWaiting: false,
 	scope: '/app',
 	sw: 'service-worker.js',
 	disable: process.env.NODE_ENV === 'development',
