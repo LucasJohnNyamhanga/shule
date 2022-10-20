7773d1fdf404c0f8f5b8c7079a4270c14c91234e
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-20 20:40:13 +0300

    trial final final

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-	register: true,
-	skipWaiting: false,
-	scope: '/app',
-	sw: 'service-worker.js',
-	disable: process.env.NODE_ENV === 'development',
-})
+const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-  // next.js config
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
 	reactStrictMode: true,
-	images: {
+  images: {
 		domains: ['database.co.tz', 'shule.co.tz', '127.0.0.1'],
 	},
-})
+});
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           name="google-site-verification"
           content="oBwl1w3_NlylQ2IpnH8227Zl6J-0vatDGgFYPsvl1zs"
         />
-        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-512x512.png" />
       </Head>
       <body>
