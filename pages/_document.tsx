e36b210996b404ed44b5e3a482923441211a023f
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-20 17:42:00 +0300

    pwa third try

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-512x512.png" />
         <meta name="theme-color" content="#fff" />
         <meta
           name="google-site-verification"
           content="oBwl1w3_NlylQ2IpnH8227Zl6J-0vatDGgFYPsvl1zs"
         />
+        <link
+          rel="manifest"
+          href="/manifest.json"
+          crossOrigin="use-credentials"
+        />
+        <link rel="apple-touch-icon" href="/icon-512x512.png" />
       </Head>
       <body>
         <Main />
