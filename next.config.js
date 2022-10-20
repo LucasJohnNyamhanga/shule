4f5ebba5e44676f192613a4f69fe8070972b7951
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-20 20:43:16 +0300

    update of update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
+})
 
 module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
+  // next.js config
 	reactStrictMode: true,
-  images: {
-		domains: ['database.co.tz', 'shule.co.tz', '127.0.0.1'],
-	},
-});
+	
+})
