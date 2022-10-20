c67b20fcaca29b4d10747aa3e892ae3c91bcc709
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-20 18:57:10 +0300

    trial 8 pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+	register: true,
+	scope: '/app',
+	sw: 'service-worker.js',
+	disable: process.env.NODE_ENV === 'development',
+})
 
 module.exports = withPWA({
   // next.js config
-	pwa:{dest: 'public',
-	register: true,
-	skipWaiting: true,
-	sw: 'service-worker.js',
-		disable: process.env.NODE_ENV === 'development',
-	},
 	reactStrictMode: true,
 	images: {
 		domains: ['database.co.tz', 'shule.co.tz', '127.0.0.1'],
