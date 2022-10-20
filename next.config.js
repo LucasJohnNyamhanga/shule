f64926854e94bb2da14d72e3565b4d09288b9bcd
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-20 20:58:40 +0300

    last knight

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
+	register: true,
 	disable: process.env.NODE_ENV === 'development',
 })
 
 module.exports = withPWA({
   // next.js config
 	reactStrictMode: true,
-	
+	images: {
+		domains: ['database.co.tz', 'shule.co.tz', '127.0.0.1'],
+	},
 })
