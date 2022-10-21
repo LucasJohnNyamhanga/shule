ed027603acbf1dfe029f4aa304074385aaeec198
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-21 08:36:35 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-SV4LGJQ10K"
+        />
+        <Script strategy="lazyOnload">{`
+          window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SV4LGJQ10K');
+        `}</Script>
+
         <meta name="theme-color" content="#fff" />
         <meta
           name="google-site-verification"
