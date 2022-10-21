92dd9ad78f246fb5a8702225d8b326cee1722cc0
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-21 11:44:22 +0300

    update offline

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -6,8 +6,8 @@ const _offline = () => {
     <div className={Styles.container}>
       <div className={Styles.innerContainer}>
         <div className={Styles.sms}>
-          <h2>Sorry, you are offline.</h2>
-          <div>
+          <div className={Styles.header}>Sorry, you are offline.</div>
+          <div className={Styles.text}>
             When you are online and view this page, it will be saved
             automatically for offline viewing.
           </div>
