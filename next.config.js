966b4f6d824a9eb0e291f01fae0423a738806c2c
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-10-25 08:37:45 +0300

    cache on front end

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')({
     register: true,
 	skipWaiting: true,
 	importScripts: ['/worker.js'],
+	cacheOnFrontEndNav: true,
 	disable:process.env.NODE_ENV === 'development'
     
 })
