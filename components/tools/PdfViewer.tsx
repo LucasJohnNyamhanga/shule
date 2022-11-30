8b9e103d570ff2e37ee4ec7266db27da36c11822
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-11-30 15:47:37 +0300

    Update PdfViewer.tsx

--- components/tools/PdfViewer.tsx
+++ components/tools/PdfViewer.tsx
@@ -1,27 +1,27 @@
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import React, { useState } from 'react';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import React, { useState } from "react";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { type } from 'os';
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { type } from "os";
 
 type dataType = {
-	url: string;
+  url: string;
 };
 
 export default function pdf({ url }: dataType) {
-	const [pageNumber, setPageNumber] = useState(1);
-	const defaultLayoutPluginInstance = defaultLayoutPlugin();
-	function onItemClick({ pageNumber: itemPageNumber }: any) {
-		setPageNumber(itemPageNumber);
-	}
+  const [pageNumber, setPageNumber] = useState(1);
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  function onItemClick({ pageNumber: itemPageNumber }: any) {
+    setPageNumber(itemPageNumber);
+  }
 
-	return (
-		<Worker workerUrl='https://unpkg.com/pdfjs-dist@2.14.305/build/pdf.worker.min.js'>
-			<div style={{ height: '750px' }}>
-				<Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
-			</div>
-		</Worker>
-	);
+  return (
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
+      <div style={{ height: "750px" }}>
+        <Viewer fileUrl={url} plugins={[defaultLayoutPluginInstance]} />
+      </div>
+    </Worker>
+  );
 }
