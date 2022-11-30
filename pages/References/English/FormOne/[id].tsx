20d9da060d297c9d6317a7daaddc3ffd969b1e68
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-11-30 15:25:55 +0300

    English

---
+++ pages/References/English/FormOne/[id].tsx
@@ -0,0 +1,154 @@
+import { GetStaticProps, GetStaticPaths, InferGetStaticPropsType } from "next";
+import { prisma } from "../../../../db/prisma";
+import { reference } from "@prisma/client";
+import React, { useContext, useEffect } from "react";
+import Styles from "../../../../styles/reviewDisplay.module.scss";
+import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
+import Head from "next/head";
+import { NavContext } from "../../../../components/context/StateContext";
+
+import PdfViewer from "../../../../components/tools/PdfViewer";
+import Link from "next/link";
+
+const subjectLocator = "English";
+const formLocator = "Form One";
+const subjectLocatorLink = "English";
+const formLocatorLink = "FormOne";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params?.id;
+  let Id = parseInt(String(id));
+  // ...
+  const referenceServer = await prisma.reference.findUnique({
+    where: {
+      id: Id,
+    },
+    select: {
+      id: true,
+      formReference: {
+        select: {
+          formName: true,
+        },
+      },
+      subjectReference: {
+        select: {
+          subjectName: true,
+        },
+      },
+      name: true,
+      description: true,
+      isPdf: true,
+      data: true,
+    },
+  });
+
+  const reference = JSON.parse(JSON.stringify(referenceServer));
+
+  return {
+    props: {
+      reference,
+    },
+    revalidate: 15,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // ...
+  const referencesServer = await prisma.reference.findMany({
+    where: {
+      published: true,
+      subjectReference: {
+        subjectName: subjectLocator,
+      },
+      formReference: {
+        some: {
+          formName: formLocator,
+        },
+      },
+    },
+    select: {
+      id: true,
+    },
+  });
+  const references = JSON.parse(JSON.stringify(referencesServer));
+
+  const paths = references.map((ref: reference) => {
+    let id = String(ref.id);
+    return {
+      params: {
+        id: `${id}`,
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+const Index = ({
+  reference,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { navActive, setNavActive } = useContext(NavContext);
+
+  useEffect(() => {
+    setNavActive("References");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [navActive]);
+
+  //!mambo yanaanza
+
+  return (
+    <div className={Styles.container}>
+      <Head>
+        <title>{reference.name}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={reference.description} />
+        {/* //!add keywords */}
+        <meta name="keywords" content={reference.description} />
+      </Head>
+      <div className={Styles.innerContainer}>
+        <div className={Styles.rightInnercontainerBody}>
+          <div className={Styles.BodyHeader}>
+            <div className={Styles.statusBar}>
+              {reference.subjectReference.subjectName}{" "}
+              <ChevronRightOutlinedIcon /> {reference.name}
+            </div>
+            {reference.isPdf && (
+              <>
+                <Link href={`/References/Download?id=${reference.id}`} passHref>
+                  <a>
+                    <div className={Styles.download}>Download</div>
+                  </a>
+                </Link>
+              </>
+            )}
+          </div>
+          <div className={Styles.BodyContent}>
+            {reference.isPdf && (
+              <div className={Styles.headerHead}>
+                <div className={Styles.header}>Book Description</div>
+                <div className={Styles.description}>
+                  {reference.description}
+                </div>
+              </div>
+            )}
+            <div>
+              {reference.isPdf ? (
+                <PdfViewer url={`/min.pdf`} />
+              ) : (
+                <div
+                  className="ckContent"
+                  dangerouslySetInnerHTML={{ __html: reference.data }}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
---
+++ pages/References/English/FormThree/index.tsx
@@ -0,0 +1,130 @@
+import React, { useContext, useEffect } from "react";
+import Styles from "../../../../styles/reviewDisplay.module.scss";
+import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
+import { prisma } from "../../../../db/prisma";
+import type { GetStaticProps, InferGetStaticPropsType } from "next";
+import Head from "next/head";
+import { NavContext } from "../../../../components/context/StateContext";
+import Book from "../../../../components/tools/Book";
+
+const subjectLocator = "English";
+const formLocator = "Form Three";
+const subjectLocatorLink = "English";
+const formLocatorLink = "FormThree";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const referenceFromServer = await prisma.reference.findMany({
+    where: {
+      published: true,
+      subjectReference: {
+        subjectName: subjectLocator,
+      },
+      formReference: {
+        some: {
+          formName: formLocator,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      formReference: {
+        select: {
+          formName: true,
+        },
+      },
+      subjectReference: {
+        select: {
+          subjectName: true,
+        },
+      },
+      name: true,
+      description: true,
+      isPdf: true,
+    },
+  });
+  const reference = JSON.parse(JSON.stringify(referenceFromServer));
+
+  return {
+    props: {
+      reference,
+    },
+    revalidate: 15,
+  };
+};
+
+type tableKey = {
+  keys: string[];
+};
+
+const Index = ({
+  reference,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { navActive, setNavActive } = useContext(NavContext);
+
+  useEffect(() => {
+    setNavActive("References");
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [navActive]);
+
+  if (reference.length < 1) {
+    return (
+      <div className={Styles.container}>
+        <div className={Styles.innerContainer}>
+          <div className={Styles.rightInnercontainerBody}>
+            <div className={Styles.BodyHeader}></div>
+            <div className={Styles.BodyContent}>
+              <div className={Styles.conteinerTable}>
+                <h2>Contents will be published soon.</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  //!mambo yanaanza
+
+  return (
+    <div className={Styles.container}>
+      <Head>
+        <title>{reference[0].topicName}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={reference[0].topicDefinition} />
+        <meta name="keywords" content={reference[0].topicName} />
+      </Head>
+      <div className={Styles.innerContainer}>
+        <div className={Styles.rightInnercontainerBody}>
+          <div className={Styles.BodyHeader}>
+            <div className={Styles.statusBar}>
+              {subjectLocator} <ChevronRightOutlinedIcon /> {formLocator}
+            </div>
+          </div>
+          <div className={Styles.BodyContent}>
+            <div className={Styles.conteinerTable}>
+              <Book
+                reference={reference}
+                link={`/References/${subjectLocatorLink}/${formLocatorLink}`}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
+
+// //*Removing default search bar :)
+// Index.getLayout = function PageLayout(page:ReactNode) {
+//   return (
+//       <>
+//           {page}
+//       </>
+//   )
+// }
