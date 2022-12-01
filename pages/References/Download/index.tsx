9aa30f0e739265661f33539b686c333ae7e8bba1
Lucas John <59226592+LucasJohnNyamhanga@users.noreply.github.com>
2022-12-01 12:21:01 +0300

    Update index.tsx

--- pages/References/Download/index.tsx
+++ pages/References/Download/index.tsx
@@ -113,9 +113,9 @@ const Index = ({
         if (name === "booksDownload") {
           if (value > 0) {
             let fileName = link.replace(/(.*)\//g, "");
-            // FileSaver.saveAs(link, fileName);
+            FileSaver.saveAs(link, "test.pdf");
 
-            startDownload(link, fileName);
+            //startDownload(link, fileName);
 
             notifySuccess("Download has started.");
             //!call decrement code
